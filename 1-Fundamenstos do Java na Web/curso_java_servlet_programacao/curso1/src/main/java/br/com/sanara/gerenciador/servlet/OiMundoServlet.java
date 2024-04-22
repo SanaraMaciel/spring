9561642706cd2974565bc482af0700342c5cdf14
sanara <sanara@cpqd.com.br>
2024-04-22 09:19:50 -0300

    inversão de controle IOC

--- 1-Fundamenstos do Java na Web/curso_java_servlet_programacao/curso1/src/main/java/br/com/sanara/gerenciador/servlet/OiMundoServlet.java
+++ 1-Fundamenstos do Java na Web/curso_java_servlet_programacao/curso1/src/main/java/br/com/sanara/gerenciador/servlet/OiMundoServlet.java
@@ -8,12 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 //@WebServlet(urlPatterns = "/oi")
+//Tomcat só instancia a servlet quando realmente ocorre uma requisição. 
+//Isso é o comportamento padrão que podemos alterar facilmente! A anotação loadOnStartup
+//@WebServlet(urlPatterns="/oi", loadOnStartup=1)
 public class OiMundoServlet extends HttpServlet {
+
+	public OiMundoServlet() {
+		System.out.println("Criando Oi Mundo Servlet");
+	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		//devolver a resposta pro navegador em caracteres
+
+		// devolver a resposta pro navegador em caracteres
 		PrintWriter out = resp.getWriter();
 		out.println("<html>");
 		out.println("<body>");
