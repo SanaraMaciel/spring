cf5174c5c54905a8c9ce8251a33a8c44ac584d45
sanara <sanara@cpqd.com.br>
2024-04-24 14:48:07 -0300

    filtro de monitoramento

--- 1-Fundamenstos do Java na Web/curso_java_servlet_programacao/curso2/gerenciador/src/main/java/br/com/sanara/gerenciador/acao/ListaEmpresas.java
+++ 1-Fundamenstos do Java na Web/curso_java_servlet_programacao/curso2/gerenciador/src/main/java/br/com/sanara/gerenciador/acao/ListaEmpresas.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.sanara.gerenciador.modelo.Banco;
 import br.com.sanara.gerenciador.modelo.Empresa;
 
-public class ListaEmpresas implements Acao{
+public class ListaEmpresas implements Acao {
 
-	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 
 		request.setAttribute("empresas", lista);
-		
+
 		return "forward:listaEmpresas.jsp";
 
 	}
---
+++ 1-Fundamenstos do Java na Web/curso_java_servlet_programacao/curso2/gerenciador/src/main/java/br/com/sanara/gerenciador/servlet/MonitoramentoFilter.java
@@ -0,0 +1,37 @@
+package br.com.sanara.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+//@WebFilter(urlPatterns="/entrada")
+@WebFilter("/entrada")
+public class MonitoramentoFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		// medindo o tempo de execução
+		long antes = System.currentTimeMillis();
+		
+		String acao = request.getParameter("acao");
+		
+		
+		//executa a acao
+		chain.doFilter(request, response);
+		
+
+		// medindo o tempo de execução
+		long depois = System.currentTimeMillis();
+
+		System.out.println("tempo de execução da ação: " + acao + (depois - antes));
+
+	}
+
+}
