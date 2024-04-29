7ad726c6eb31a5ff1e78de311eeb129533cc3d64
sanara <sanara@cpqd.com.br>
2024-04-29 10:09:20 -0300

    cliente do serviço

--- 1-Fundamenstos do Java na Web/curso_java_servlet_programacao/curso2/gerenciador/src/main/java/br/com/sanara/gerenciador/servlet/EmpresaService.java
+++ 1-Fundamenstos do Java na Web/curso_java_servlet_programacao/curso2/gerenciador/src/main/java/br/com/sanara/gerenciador/servlet/EmpresaService.java
@@ -10,26 +10,61 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.sanara.gerenciador.modelo.Banco;
 import br.com.sanara.gerenciador.modelo.Empresa;
 
-
 @WebServlet("/empresas")
 public class EmpresaService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		 List<Empresa> empresas = new Banco().getEmpresas();
-		 
-		 Gson gson = new Gson();
-		 String json = gson.toJson(empresas);
-		 
-		 response.setContentType("application/json");
-		 response.getWriter().print(json);
-		 
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		/*
+		 * List<Empresa> empresas = new Banco().getEmpresas();
+		 * 
+		 * // devolvendo um xml /*XStream xstream = new XStream();
+		 * xstream.alias("empresa", Empresa.class); String xml =
+		 * xstream.toXML(empresas);
+		 * 
+		 * response.setContentType("application/xml"); response.getWriter().print(xml);
+		 */
+
+		// devolvendo um json
+		/*
+		 * Gson gson = new Gson(); String json = gson.toJson(empresas);
+		 * 
+		 * response.setContentType("application/json");
+		 * response.getWriter().print(json);
+		 */
+
+		List<Empresa> empresas = new Banco().getEmpresas();
+
+		String valor = request.getHeader("Accept");
+
+		if (valor.contains("xml")) {
+
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+
+		} else if (valor.endsWith("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message':'no content'}");
+		}
+
 	}
 
 }
