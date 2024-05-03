8e76241196bfcc0d55d68c383e403c135b2519c6
sanara <sanara@cpqd.com.br>
2024-05-03 11:53:40 -0300

    separando a aplicação em modulos

---
+++ 1-Fundamenstos do Java na Web/maven/meteora/produtos/src/main/java/br/com/sanara/Main.java
@@ -0,0 +1,30 @@
+package br.com.sanara;
+
+import br.com.sanara.model.Produto;
+import br.com.sanara.service.TraduzProdutoService;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //importa uma lista de produtos em formato csv e faz o parse p/ um obj java produto
+        List<Produto> produtos = new CsvToBeanBuilder(new FileReader("src/main/resources/products.csv")).withType(Produto.class).build().parse();
+
+
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
+
+        TraduzProdutoService traducaoService = new TraduzProdutoService();
+        for (Produto produto : produtos) {
+            traducaoService.traduzir(produto);
+            System.out.println(produto);
+        }
+
+    }
+}
---
+++ 1-Fundamenstos do Java na Web/maven/meteora/produtos/src/main/java/br/com/sanara/service/TraduzProdutoService.java
@@ -0,0 +1,22 @@
+package br.com.sanara.service;
+
+import br.com.sanara.model.Produto;
+import net.suuft.libretranslate.Language;
+import net.suuft.libretranslate.Translator;
+
+public class TraduzProdutoService {
+
+    public void traduzir(Produto produto) {
+
+        String novoNome = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getName());
+        String novaCategoria = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getCategory());
+        String novaDescricao = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getDescription());
+
+        produto.setName(novoNome);
+        produto.setCategory(novaCategoria);
+        produto.setDescription(novaDescricao);
+
+    }
+
+
+}
