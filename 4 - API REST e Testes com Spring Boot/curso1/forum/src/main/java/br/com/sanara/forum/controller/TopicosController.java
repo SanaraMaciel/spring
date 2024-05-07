7028ad48101ee04e36138978d97f9f3ab22c0f55
sanara <sanara@cpqd.com.br>
2024-05-07 10:04:15 -0300

    adicionada configuração de banco de dados, repository e lista

--- 4 - API REST e Testes com Spring Boot/curso1/forum/src/main/java/br/com/sanara/forum/controller/TopicosController.java
+++ 4 - API REST e Testes com Spring Boot/curso1/forum/src/main/java/br/com/sanara/forum/controller/TopicosController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import br.com.sanara.forum.controller.dto.TopicoDto;
+import br.com.sanara.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,29 @@ import br.com.sanara.forum.modelo.Topico;
 
 @RestController
 public class TopicosController {
-	
+	/*
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
 		Topico topico = new Topico("Dúvida", "Dúvida com Spring2", new Curso("Spring", "Programação"));
 		
 		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
-	}
+	}*/
+
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+
+    /**
+     * retorna os dados vindo do banco de dados - topicos
+     *
+     * @return lista de topicos
+     */
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista() {
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
+    }
+
 
 }
--- 4 - API REST e Testes com Spring Boot/curso1/forum/src/main/java/br/com/sanara/forum/modelo/Resposta.java
+++ 4 - API REST e Testes com Spring Boot/curso1/forum/src/main/java/br/com/sanara/forum/modelo/Resposta.java
@@ -1,15 +1,29 @@
 package br.com.sanara.forum.modelo;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String mensagem;
+
+	@ManyToOne
 	private Topico topico;
+
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+
+	@ManyToOne
 	private Usuario autor;
+
 	private Boolean solucao = false;
+
+	public Resposta() {
+	}
 
 	@Override
 	public int hashCode() {
