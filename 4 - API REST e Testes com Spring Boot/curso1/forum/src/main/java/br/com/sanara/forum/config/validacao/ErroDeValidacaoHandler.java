adb0ec7d6cd6c39a5fd2556263a1a501e920be17
sanara <sanara@cpqd.com.br>
2024-05-07 11:28:12 -0300

    personalizando validação de erro com Handler

---
+++ 4 - API REST e Testes com Spring Boot/curso1/forum/src/main/java/br/com/sanara/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -0,0 +1,47 @@
+package br.com.sanara.forum.config.validacao;
+
+import org.aspectj.bridge.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ErroDeValidacaoHandler {
+
+
+    //classe do spring que te ajuda a pegar msg de erros de acordo com o idioma
+    @Autowired
+    private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErroDeFormularioDto> handle(MethodArgumentNotValidException exception) {
+
+        List<ErroDeFormularioDto> dto = new ArrayList<>();
+
+        //método que contém todos os erros de campo do formulario
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        //transformar cada erro em um erroDTO
+        fieldErrors.forEach(e -> {
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ErroDeFormularioDto erro = new ErroDeFormularioDto(e.getField(), mensagem);
+            dto.add(erro);
+        });
+
+        return dto;
+
+    }
+
+}
+
+
