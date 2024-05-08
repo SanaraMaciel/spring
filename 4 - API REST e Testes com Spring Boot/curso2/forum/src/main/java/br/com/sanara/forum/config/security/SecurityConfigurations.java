918edda5545a7426a5692923df02267129fe4b12
sanara <sanara@cpqd.com.br>
2024-05-08 17:30:23 -0300

    Gerando token com JWT

--- 4 - API REST e Testes com Spring Boot/curso2/forum/src/main/java/br/com/sanara/forum/config/security/SecurityConfigurations.java
+++ 4 - API REST e Testes com Spring Boot/curso2/forum/src/main/java/br/com/sanara/forum/config/security/SecurityConfigurations.java
@@ -1,13 +1,16 @@
 package br.com.sanara.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -17,6 +20,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Autowired
     private AutenticacaoService autenticacaoService;
 
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     //configuracoes de autenticacao
     @Override
@@ -30,8 +39,14 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin();//diz ao spring pra gerar um formulario de autenticacao
+                .and().csrf().disable() //desabilitado p/ o Spring security não fazer a validação do token do csrf.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//informa ao spring p/ n criar sessao
+
+
+                //comentado p/ o spring não usar sessão
+                //.and().formLogin();//diz ao spring pra gerar um formulario de autenticacao
     }
 
     //configuracoes de recursos estaticos(js,css, imagens,etc.)
---
+++ 4 - API REST e Testes com Spring Boot/curso2/forum/src/main/java/br/com/sanara/forum/config/security/TokenService.java
@@ -0,0 +1,36 @@
+package br.com.sanara.forum.config.security;
+
+import br.com.sanara.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication) {
+        //pega o usuario que esta logado
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API fórum Alura")
+                .setSubject(logado.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256,secret)
+                .compact();
+
+    }
+}
