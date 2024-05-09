cd89322a19dc63f26bb3851d33ec781eb6ab16a8
sanara <sanara@cpqd.com.br>
2024-05-09 16:37:05 -0300

    trabaçhando com profiles de dev e prod

---
+++ 4 - API REST e Testes com Spring Boot/curso3/forum/src/main/java/br/com/sanara/forum/config/security/DevSecurityConfigurations.java
@@ -0,0 +1,27 @@
+package br.com.sanara.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("dev")//faz com que o spring so carregue essa classe qdo tiver no perfil de dev
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().csrf().disable();
+    }
+}
+
+
+
+
+
+
+
